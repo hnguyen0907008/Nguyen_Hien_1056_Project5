f941f3333875cdb5e16eadef28eec82db27ed6b3
Nguyen <h_nguyen90092@TSS-F1000-MAC11.fanshawec.ca>
2019-07-23 02:09:58 -0400

    toggle

---
+++ js/main.js
@@ -0,0 +1,25 @@
+(() => {
+	console.log('fired');
+	let mobiNav = document.querySelector('.nav'),
+		navPanel = document.querySelector('#burgerCon');
+		navLinks = navPanel.querySelectorAll('a');
+	function toggleNav(event) {
+		event.preventDefault();
+		console.log ('nav dropdown');
+
+		if (this.nodeName == "A") {
+			debugger;
+			//click anchor tag to fo some navigation
+			window.scrollTo({
+				//ask window to scroll to an element on page
+				top: this.offsetTop, 
+				behavior: 'smooth'
+			})
+		}
+
+		navPanel.classList.toggle('show-nav');
+	}
+
+	mobiNav.addEventListener('click', toggleNav);
+	navLinks.forEach(link => link.addEventListener('click', toggleNav));
+})();
